ff3162d3d3bebe15eee253ace33beb09cbc9eb86
Antavatech <37000409+Antavatech@users.noreply.github.com>
2023-09-07 15:08:27 -0400

    apt upgrade

--- Myapps.sh
+++ Myapps.sh
@@ -7,7 +7,8 @@ x=$(cat h.txt)
 echo " " >> $x/ATech.log
 date >> $x/ATech.log
 echo "Updating apt..." >> $x/ATech.log && sudo apt-get update
-echo "Installing Stress..." >> $x/ATech && sudo apt-get install stress
+echo "Upgrading software..." >> $x/ATech.log && sudo apt-get upgrade -y
+# echo "Installing Stress..." >> $x/ATech.log && sudo apt-get install stress
 echo "Completed at..." >> $x/ATech.log
 date >> $x/ATech.log
 rm -rf h.txt
