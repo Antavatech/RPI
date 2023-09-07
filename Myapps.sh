64f978f58bb3240fa4a2b6f7e971e3f3cdc1d76a
Antavatech <37000409+Antavatech@users.noreply.github.com>
2023-09-07 15:10:55 -0400

    Update Myapps.sh

--- Myapps.sh
+++ Myapps.sh
@@ -9,6 +9,5 @@ date >> $x/ATech.log
 echo "Updating apt..." >> $x/ATech.log && sudo apt-get update
 echo "Upgrading software..." >> $x/ATech.log && sudo apt-get upgrade -y
 # echo "Installing Stress..." >> $x/ATech.log && sudo apt-get install stress
-echo "Completed at..." >> $x/ATech.log
-date >> $x/ATech.log
+echo "Completed at..." && date >> $x/ATech.log
 rm -rf h.txt
