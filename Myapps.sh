c5d2db4de0f9f37ac000f7d9e734dc6eee444301
Antavatech <37000409+Antavatech@users.noreply.github.com>
2023-09-10 13:50:37 -0400

    Update

--- Myapps.sh
+++ Myapps.sh
@@ -7,7 +7,7 @@ x=$(cat home.txt)
 echo " " >> $x/ATech.log
 date >> $x/ATech.log
 echo "Updating apt..." >> $x/ATech.log && sudo apt-get update
-echo "Upgrading software..." >> $x/ATech.log && sudo apt-get upgrade -y
-# echo "Installing Stress..." >> $x/ATech.log && sudo apt-get install stress
+# echo "Upgrading software..." >> $x/ATech.log && sudo apt-get upgrade -y
+echo "Installing Stress..." >> $x/ATech.log && sudo apt-get install stress -y
 echo "Completed at..." >> $x/ATech.log && date >> $x/ATech.log
 rm -rf home.txt
